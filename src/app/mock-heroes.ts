5fd0c49e9538f8fdd69754c7541022a4f50f4073
yashthakur0 <yashthakur1915@gmail.com>
2022-04-05 14:29:39 +0530

    update 1.8

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+    {id: 1, name: 'Iron Man'},
+    {id: 2, name: 'Spider Man'},
+    {id: 3, name: 'Black Widow'},
+    {id: 4, name: 'Shanchi'},
+    {id: 5, name: 'Flash'},
+    {id: 6, name: 'BatMan'},
+    {id: 7, name: 'Aqua Man'},
+    {id: 8, name: 'Ant Man'},
+    {id: 9, name: 'Moon Knight'},
+    {id: 10, name: 'Wonder Women'}
+];
