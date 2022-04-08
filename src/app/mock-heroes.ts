7175d76caabf5e95883a2fd0c320661a46f56389
yashthakur0 <yash.thakur@ceryxdigital.com>
2022-04-08 16:15:25 +0530

    #2 is completed

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,7 +4,7 @@ export const HEROES : Hero[] = [
     {id: 1, name: 'Iron Man'},
     {id: 2, name: 'Spider Man'},
     {id: 3, name: 'Black Widow'},
-    {id: 4, name: 'Shanchi'},
+    {id: 4, name: 'Shagnchi'},
     {id: 5, name: 'Flash'},
     {id: 6, name: 'BatMan'},
     {id: 7, name: 'Aqua Man'},
