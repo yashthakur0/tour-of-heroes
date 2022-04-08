d07fcfcfd5714470e0260ee18878f0f54507abc0
yashthakur0 <yash.thakur@ceryxdigital.com>
2022-04-08 16:03:38 +0530

    css issue solved

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,21 +7,18 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero : Hero = {
-    id : 1,
-    name : 'Windstorm'    
-  };
-
   heroes = HEROES;
-
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+
+  constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
